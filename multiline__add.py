32cc2848165f87d941eecc29553f0bd1d1b0dc77
Suryash Rastogi <42309694+Suryash-00@users.noreply.github.com>
2020-03-16 22:16:56 -0700

    Create multiline__add.py

---
+++ multiline__add.py
@@ -0,0 +1,19 @@
+"""
+multistrings can be used as
+comments too !
+"""
+multi = """ 
+hello!
+my name is Suryash Rastogi
+"""
+print(multi)
+
+
+name= "Suryash"
+greetings= "hello " + name
+print(greetings)
+
+
+age=20
+age_as_string= str(age)               #strings can only be concanecated with another string. so conversion is done
+print("you are " + age_as_string)
