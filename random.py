f72264281350775d342e72255b4d16a68a0636d3
Suryash-00 <suryash35@gmail.com>
2021-11-11 23:22:13 +0530

    python

--- random.py
+++ random.py
@@ -1,26 +1,18 @@
-def runner_up():
-    arr=[]
-    n = int(input("Enter subjects"))
-    for i in range(0, n):
-        ele = int(input("Enter marks"))
-        arr.append(ele)
-    print(arr)
+arr=[]
+n = int(input("Enter subjects"))
+for i in range(0, n):
+    ele = int(input("Enter marks"))
+    arr.append(ele)
+for i in range(0, n):
+    for j in range(0, n - i - 1):
+        if arr[j] > arr[j + 1]:
+            temp = arr[j]
+            arr[j] = arr[j + 1]
+            arr[j + 1] = temp
 
-    for i in range(n):
-        for j in range(n):
-            if arr[j] < arr[j+1]:
-                temp=arr[j]
-                arr[j]=arr[j+1]
-                arr[j+1]=temp
-
-    for i in range(0, n):
-        print(arr[i])
-
+while n != 0:
     i = 1
-    while n != 0:
-        i = i + 1
-        n = n - 1
-        if arr[n - i] < arr[n - 1]:
-            print(arr[n - i])
-
-runner_up()
+    n = n - 1
+    if arr[n - i] < arr[n]:
+        print(arr[n - i])
+        break
